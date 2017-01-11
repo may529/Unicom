ec8da226580789e45b3863dbb824e30ac796446b
isHidden <isHidden@hotmail.com>
2017-01-11 13:53:35 +0800

    修改地址

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,7 +4,8 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
-  host:window.location.protocol + "//" +window.location.host+'/'
+  // host:window.location.protocol + "//" +window.location.host+'/'
+  host:"https://unicom.parsec.com.cn/test/"
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
